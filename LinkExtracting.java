d5dbe0a8cd383755b8bbd0ca51374ee45286aa65
marilenavelonia <marvelonia@windowslive.com>
2016-11-24 21:29:43 +0300

    Add files via upload

--- LinkExtracting.java
+++ LinkExtracting.java
@@ -1,41 +1,65 @@
-  import java.util.regex.Pattern;
-  import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
+public class LinkExtracting {
 
-  public class LinkExtracting {
-  	//...
-      while ((line = rd.readLine()) != null) {
-         //object result contains src of the page being crawled
-      	result.append(line);
+	private String link, linkText;
+	
+	//method description
+	public boolean extractLinks (String line, String baseURL) {
+    	//i tests if the method returns true or false
+      	int i = 0;
+
+      	Pattern tagPattern, linkPattern;
+      	Matcher tagMatcher, linkMatcher; 
 
       	//<a tag pattern that indicates a link in html code
-      	public final String ATagPattern = "(?i)<a([^>]+)>(.*?)</a>";
+      	final String ATagPattern = "(?i)<a([^>]+)>(.*?)</a>";
       	//<a href pattern that is a URL
-      	public final String AHrefTagPattern = "\\s*(?i)href\\s*=\\s*(\"([^\"]*\")|'[^']*'|([^'\">\\s]+))";
+      	final String AHrefTagPattern = "\\s*(?i)href\\s*=\\s*(\"([^\"]*\")|'[^']*'|([^'\">\\s]+))";
+		
 
       	tagPattern = Pattern.compile(ATagPattern);
       	linkPattern = Pattern.compile(AHrefTagPattern);
+      	
 
       	tagMatcher = tagPattern.matcher(line);
 
       	//while the <a pattern exists in line, extract the href and the link text from the html link line
       	while (tagMatcher.find()) {
       		String href = tagMatcher.group(1);
-      		String linkText = tagMacher.group(2);
+      		linkText = tagMatcher.group(2);
 
       		linkMatcher = linkPattern.matcher(href);
 
-      			//while the href pattern matches the href String, extract the URL
-      			while (linkMatcher.find()) {
-      				String link = linkMatcher.group(1);
+      		//while the href pattern matches the href String, extract the URL
+      		while (linkMatcher.find()) {
+      			link = linkMatcher.group(1);
 
-					//if line does not represent an internal link, an email link or a document-download link then save link and text link in database
-					if ((line.contains("<\P>") == false) && (line.contains("@") == false) && (line.contains("zip") == false)) {
-      				//save sti vasi dedomenwn tou link kai tou linktext
-      				}
-      			}
-      	}
-      }
+      			
+
+      			//if the link is external or local, return true
+      			if ((link.contains("www") || link.contains("http") || link.startsWith("//")) || link.startsWith("/")) {
+				    i = 1;
+				    if (link.startsWith("/") == true && link.startsWith("//") == false) {
+						link = baseURL + link;}
+				}
+			}
+		}
+
+		if (i == 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public String getLink() {
+		return link;
+	}
+	
+	public String getLinkText(){
+		return linkText;
+	}
 }
-
 
